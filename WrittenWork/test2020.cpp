13c5f0976eba566e0ec87738cb6c19e2ab131d71
1shaked <shaked1hen@gmail.com>
2022-01-31 19:33:35 +0200

    done

---
+++ WrittenWork/test2020.cpp
@@ -0,0 +1,8 @@
+//
+//  test2020.cpp
+//  WrittenWork
+//
+//  Created by shaked chen on 30/01/2022.
+//
+
+#include "test2020.hpp"
---
+++ test2020.hpp
@@ -0,0 +1,13 @@
+//
+//  test2020.hpp
+//  WrittenWork
+//
+//  Created by shaked chen on 30/01/2022.
+//
+
+#ifndef test2020_hpp
+#define test2020_hpp
+
+#include <stdio.h>
+
+#endif /* test2020_hpp */
